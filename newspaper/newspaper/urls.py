1aa7e5acb6e3bae6a6de59aa5420ee923bd06730
Aidan <bantezea@gmail.com>
2025-10-15 09:36:13 +0300

    finalize authentication

--- newspaper/newspaper/urls.py
+++ newspaper/newspaper/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from users.views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
